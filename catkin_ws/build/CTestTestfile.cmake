173bbd327f932ad29b6d59805583b1ed35913b0f
lmymapu <lmymapu@gmail.com>
2016-12-12 20:18:42 +0100

    create new project of ros hockey game

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/julia/work/LKC++/ROS_Hockey/catkin_ws/src
+# Build directory: /home/julia/work/LKC++/ROS_Hockey/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
